18abea5598d33db1a6ae83c8b2aaadb678ef76b0
Junaid <junaidchoudhary210@gmail.com>
2019-11-06 16:07:09 +0530

    username and password changed

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 import urllib
 from pymongo import MongoClient
 
-username = 'neargroup'
-password = 'neargroup@1234'
+username = '*********'
+password = '*************'
 mongoURI = f'mongodb+srv://{urllib.parse.quote(username)}:{urllib.parse.quote(password)}@ngapp-qfeen.mongodb.net/neargroup?retryWrites=true'
 myclient = MongoClient(mongoURI, ssl_cert_reqs=ssl.CERT_NONE)
 
